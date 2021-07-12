fd91aee55cc559ddd7b926e3aed8cc1a060183be
edsheltonorg <ed@edshelton.org>
2021-07-11 22:51:10 -0500

    More math

--- html/index.php
+++ html/index.php
@@ -1,17 +1,14 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>Ed PHP Test</title>
  </head>
  <body>
  <?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
+echo "<h2>PHP</h2>";
 $a = 1;
 $b = 2;
 $sum = $a + $b;
-echo "sum: " . $sum;
+echo $a . " + " . $b . "'s sum = " . $sum;
 ?> 
  </body>
 </html>
