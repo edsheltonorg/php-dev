0464f5a59521b1e228bde3f939e2487d13e0039b
edsheltonorg <ed@edshelton.org>
2021-07-11 23:16:31 -0500

    even more math

--- html/index.php
+++ html/index.php
@@ -1,14 +1,14 @@
 <html>
- <head>
-  <title>Ed PHP Test</title>
- </head>
- <body>
- <?php
-echo "<h2>PHP</h2>";
-$a = 1;
-$b = 2;
-$sum = $a + $b;
-echo $a . " + " . $b . "'s sum = " . $sum;
-?> 
- </body>
+    <head>
+        <title>Ed PHP Test</title>
+    </head>
+<body>
+    <?php
+        echo "<h2>PHP</h2>";
+        $a = 2;
+        $b = 5;
+        $sum = $a + $b;
+        echo $a . " + " . $b . "'s sum = " . $sum;
+    ?>
+</body>
 </html>
