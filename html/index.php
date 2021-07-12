1d9c898f02efa077ed678ae7a5349a0ab978efc3
edsheltonorg <ed@edshelton.org>
2021-07-11 22:42:42 -0500

    php changed w/ math

--- html/index.php
+++ html/index.php
@@ -7,7 +7,11 @@
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
+$a = 1;
+$b = 2;
+$sum = $a + $b;
+echo "sum: " . $sum;
 ?> 
  </body>
 </html>
