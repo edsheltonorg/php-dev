6a63ae0d7bee2852c8e9295205c738d15c23c55d
Ed Shelton <ed@edshelton.org>
2021-07-18 19:12:39 -0500

    vscode diff test

--- html/index.php
+++ html/index.php
@@ -5,7 +5,7 @@
 <body>
     <?php
         echo "<h2>I Love You Beth</h2>";
-        $a = 2;
+        $a = 3;
         $b = 10;
         $sum = $a + $b;
         echo $a . " + " . $b . "'s sum = " . $sum;
